7d7967f51fc8f6fa0af4b12d70d73ed839179cc2
Marek Czerniejewski <marekcze@outlook.com>
2023-03-07 21:26:43 +0000

    finished basic CRUD

--- ReactivitiesSolution/API/Controllers/BaseApiController.cs
+++ ReactivitiesSolution/API/Controllers/BaseApiController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
 
 namespace API.Controllers
 {
@@ -6,6 +7,13 @@ namespace API.Controllers
 	[Route("api/[controller]")]
 	public class BaseApiController : ControllerBase
 	{
+		private IMediator _mediator;
+
+		// if _mediator is not null it will be assigned to Mediator
+		// if _mediator is null then what's on the right of ??= will be
+		// assigned to Mediator
+		protected IMediator Mediator => _mediator ??=
+			HttpContext.RequestServices.GetService<IMediator>();
 
 	}
 }
---
+++ ReactivitiesSolution/API/Extensions/ApplicationServiceExtensions.cs
@@ -0,0 +1,38 @@
+﻿using Application.Activities;
+using Application.Core;
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+
+namespace API.Extensions
+{
+	public static class ApplicationServiceExtensions
+	{
+		public static IServiceCollection AddApplicationServices(this IServiceCollection services,
+			IConfiguration config)
+		{
+			// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+			services.AddEndpointsApiExplorer();
+			services.AddSwaggerGen();
+			services.AddDbContext<DataContext>(opt =>
+			{
+				opt.UseSqlite(config.GetConnectionString("DefaultConnection"));
+			});
+			services.AddCors(opt =>
+			{
+				opt.AddPolicy("CorsPolicy", policy =>
+				{
+					policy.AllowAnyMethod().AllowAnyHeader().WithOrigins("http://localhost:3000");
+				});
+			});
+
+			// have to use Action type config with new MediatR version instead of what's in the Udemy course
+			// tutorial with code: https://code-maze.com/cqrs-mediatr-in-aspnet-core/
+			services.AddMediatR(cfg =>
+			cfg.RegisterServicesFromAssembly(typeof(List.Handler).Assembly));
+
+			services.AddAutoMapper(typeof(MappingProfiles).Assembly);
+
+			return services;
+		}
+	}
+}
